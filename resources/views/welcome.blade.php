080606425e6e3c945cba0df27ecce3b1ff9d449c
Renāte Vidruska <renate.vidruska@if.lv>
2021-09-05 20:08:08 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.public')
+
+@section('content')
+<div class="py-16 px-2">
+    <div class="container max-w-screen-xl mx-auto">
+        @auth
+            <h1 class="text-2xl p-4 bg-yellow-900 text-white rounded-md">
+                Welcome, {{auth()->user()->name}}
+            </h1>
+        @endauth
+    </div>
+</div>
+@stop
